f991de6da36ac420334d9baf6b83cb0f534bcba4
harinath1314 <harinathareddy@harinathas-MacBook-Air.local>
2018-11-04 21:39:06 +0530

    "ads2_m4_a1_1: 5/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS2_assignments/m4/Code Camp - WordNet/WordNet/Solution.java
+++ ADS2_assignments/m4/Code Camp - WordNet/WordNet/Solution.java
@@ -47,6 +47,7 @@ public class Solution {
 						throw new Exception("IllegalArgumentException");
 					}
 					System.out.println("oooooooooooooooooooooo");
+					System.out.println(qyery[0]);
 					System.out.println("distance = " + wn.distance(qyery[0], qyery[1]) + ", ancestor = " + wn.sap(qyery[0], qyery[1]));
 				}
 
