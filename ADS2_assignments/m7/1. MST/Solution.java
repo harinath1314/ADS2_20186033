268390265c07e29ade908876ee4df93582adaca9
harinath1314 <harinathareddy@harinathas-MacBook-Air.local>
2018-11-05 18:11:42 +0530

    "ads2_m7_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS2_assignments/m7/1. MST/Solution.java
@@ -0,0 +1,22 @@
+/**
+ * harinatha 
+ */
+
+import java.util.Scanner;
+public final class Solution{
+	Solution(){
+
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String abb = sc.nextLine();
+		int a = Integer.parseInt(abb);
+		String baa = sc.nextLine();
+
+		int b = Integer.parseInt(baa);
+		EdgeWeightedGraph ewg = new EdgeWeightedGraph(a, b);
+		KruskalMST kmst = new KruskalMST(ewg);
+		System.out.println(kmst.weight());
+
+	}
+}
