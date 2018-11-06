4780e17d5e3a8d29c426b79da3a90929106d78cc
harinath1314 <harinathareddy@harinathas-MacBook-Air.local>
2018-11-06 18:25:20 +0530

    "ads2_m8_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS2_assignments/m8/1. Shortest Paths/Solution.java
+++ ADS2_assignments/m8/1. Shortest Paths/Solution.java
@@ -21,6 +21,7 @@ public final class Solution {
      * @param      args  The arguments
      */
     public static void main(final String[] args) {
+        // reads input from cmd.
         Scanner sc = new Scanner(System.in);
         int nofrs = sc.nextInt();
         int nofdirectpaths = sc.nextInt();
@@ -32,12 +33,14 @@ public final class Solution {
             hs.put(stations[j], j);
 
         }
-        //
+        // complexity of edgeweighted graph is E.
+        // where E is the Number of EDGES.
         EdgeWeightedGraph ewdg = new EdgeWeightedGraph(nofrs);
         for (int i = 0; i < nofdirectpaths; i++) {
             String[] edgedetails = sc.nextLine().split(" ");
             Edge de = new Edge(hs.get(edgedetails[0]),
                hs.get(edgedetails[1]), Double.parseDouble(edgedetails[2]));
+            // addEdge Has a complexity of O(1).
             ewdg.addEdge(de);
 
 
@@ -47,8 +50,12 @@ public final class Solution {
         int qyer = Integer.parseInt(sc.nextLine());
         for (int q = 0; q < qyer; q++) {
             String[] querydetails = sc.nextLine().split(" ");
-            // object for DijkstrasShortest path algorith
-            // it has a complexity of 
+            //Dijkstra's algorithm computes a SPT in any edge-weighted.
+            //digraph with nonnegative weights..
+            // object for DijkstrasShortest path algorith.
+            // it has a complexity depending upon the PQ implementation.
+            // here we used IndexPQ using Binary Heap.
+            // hence we have a complexity of. ------  "ElogV"
             DijkstraUndirectedSP dsp = new
             DijkstraUndirectedSP(ewdg, hs.get(querydetails[0]));
             System.out.println(Math.round(dsp.distTo(hs.get(querydetails[1]))));
