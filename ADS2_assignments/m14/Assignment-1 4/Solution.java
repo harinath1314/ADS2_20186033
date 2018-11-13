355d9d83b27b6428bd29d2c356d8fab42c70338e
harinath1314 <harinathareddy@harinathas-MacBook-Air.local>
2018-11-13 16:38:56 +0530

    "ads2_m14_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS2_assignments/m14/Assignment-1 4/Solution.java
+++ ADS2_assignments/m14/Assignment-1 4/Solution.java
@@ -1,12 +1,27 @@
 import java.util.Scanner;
-
+/**
+ * Class for solution.
+ */
 public class Solution {
-    public static void main(String[] args) {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+
+    }
+    /**
+     * main method starts here.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
         String[] word = loadWords();
         //Your code goes here...
+        //tst has a complexity of L + lg N for search and has lg N for miss.
         TST tst = new TST();
         for (int i = 0; i < word.length; i++) {
             int n = word[i].length();
+            // string array of all the suffixes of the given word.
             String[] suffixes = new String[n];
             for (int j = 0; j < n; j++) {
                 suffixes[j] = word[i].substring(j, n);
@@ -16,10 +31,8 @@ public class Solution {
 
         }
         Scanner sc = new Scanner(System.in);
+        // iterablr String of all the keys with prefix as given word.
         Iterable<String> st = tst.keysWithPrefix(sc.nextLine());
-        // Iterable<String> st = tst.keys();
-
-
 
         for (String s : st) {
             System.out.println(s.toString());
@@ -27,7 +40,11 @@ public class Solution {
 
 
     }
-
+    /**
+     * Loads words.
+     *
+     * @return     a String array of words.
+     */
     public static String[] loadWords() {
         In in = new In("/Files/dictionary-algs4.txt");
         String[] words = in.readAllStrings();
