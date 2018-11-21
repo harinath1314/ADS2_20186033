a02caacdff0f31120d8b582fc5d89540f5dbe575
harinath1314 <harinathareddy@harinathas-MacBook-Air.local>
2018-11-21 09:27:35 +0530

    "ads2_m10_a1_1: 9/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS2_assignments/m10/Seam Carving/SeamCarver.java
+++ ADS2_assignments/m10/Seam Carving/SeamCarver.java
@@ -1,166 +1,286 @@
 import java.awt.Color;
-import java.util.ArrayList;
+/**
+ * Class for seam carver.
+ */
 public class SeamCarver {
-	// create a seam carver object based on the given picture
-	private Picture picture;
-	private double[][] energy;
-	public SeamCarver(Picture pic) {
-		if (pic == null) {
-			throw new IllegalArgumentException("picture is null");
-		}
-		this.picture = pic;
-		energy = new double[pic.width()][pic.height()];
-		for (int col = 0; col < width(); col++) {
-			for (int row = 0; row < height(); row++) {
-				energy[col][row] = energy(col, row);
-			}
-		}
-	}
-	// current picture
-	public Picture picture() {
-		return this.picture;
-	}
-	// width of current picture
-	public int width() {
-		return energy.length;
-	}
+    /**
+     * double border.
+     */
+    private static final double BORDER = 1000;
+    /**
+     * Picture object.
+     */
+    private Picture pict;
+    /**
+     * Constructs the object.
+     * create a seam carver object based on the given picture.
+     *
+     * @param      picture  The picture
+     */
+    public SeamCarver(final Picture picture) {
+        if (picture == null) {
+            throw new IllegalArgumentException("picture is null");
+        }
+        this.pict = picture;
+    }
+    /**
+     * current picture.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public Picture picture() {
+        return pict;
+    }
+    /**
+     * width of current picture.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int width() {
+        return pict.width();
+    }
+    /**
+     * height of current picture.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int height() {
+        return pict.height();
+    }
+    /**
+     * energy of pixel at column x and row y.
+     *
+     * @param      x     { parameter_description }
+     * @param      y     { parameter_description }
+     *
+     * @return     double.
+     */
+    public double energy(final int x, final int y) {
+        int w = width() - 1, h = height() - 1;
+        if (x < 0 || x > w || y < 0 || y > h) {
+            throw new java.lang.IllegalArgumentException(
+                "IllegalArgumentException");
+        }
+        if (x == 0 || x == w ||  y == 0 || y == h) {
+            return BORDER;
+        }
+        return internalEnergy(x, y);
+    }
+    /**
+     * // energy of pixel at column x and row y not on boarder.
+     *
+     * @param      x  integer
+     * @param      y  integer
+     *
+     * @return  // energy of pixel at column x and row y not on boarder
+     */
+    private double internalEnergy(final int x, final int y) {
+        Color left = this.pict.get(x - 1, y);
+        Color right = this.pict.get(x + 1, y);
+        Color up = this.pict.get(x, y - 1);
+        Color down = this.pict.get(x, y + 1);
+        return Math.sqrt(gradient(left, right) + gradient(up, down));
+    }
+    /**
+     * gradient method.
+     *
+     * @param      one   One
+     * @param      two   Two
+     *
+     * @return  gradient horizontal and vertical.
+     */
+    private double gradient(final Color one, final Color two) {
+        double red = one.getRed() - two.getRed();
+        double green = one.getGreen() - two.getGreen();
+        double blue = one.getBlue() - two.getBlue();
+        return red * red + green * green + blue * blue;
+    }
+    /**
+     * energy storage.
+     *
+     * @return  energies
+     */
+    private double[][] initEnergies() {
+        double[][] energies = new double[height()][width()];
+        for (int i = 0; i < height(); i++) {
+            for (int j = 0; j < width(); j++) {
+                energies[i][j] = energy(j, i);
+            }
+        }
+        return energies;
+    }
+    /**
+     *  // pass through an array and mark the.
+     *   shorthest distance from top to entry.
+     *
+     * @param      energies  The energies
+     */
 
-	// height of current picture
-	public int height() {
-		return energy[0].length;
-	}
-	public void relaxEdge(int fromPixel, int toPixel, double[] distTo, int[] edgeTo) {
+    private void topologicalSort(final double[][] energies) {
+        int h = energies.length, w = energies[0].length;
+        for (int row = 1; row < h; row++) {
+            for (int col = 0; col < w; col++) {
+                double temp = energies[row - 1][col];
+                double min = 0;
+                if (col == 0) {
+                    min = temp;
+                } else {
+                    min = Math.min(temp, energies[row - 1][col - 1]);
+                }
 
-		if (distTo[fromPixel] + energy[toPixel % width()][toPixel / width()] <
-		        distTo[toPixel]) {
-			distTo[toPixel] = distTo[fromPixel] +
-			                  energy[toPixel % width()][toPixel / width()];
-			edgeTo[toPixel] = fromPixel;
-		}
-	}
-	// energy of pixel at column x and row y
-	public double energy(int x, int y) {
-		double dx = 0.0;
-		double dy = 0.0;
-		Color front;
-		Color back;
-		if (x < 0 || x >= width() || y < 0 || y >= height()) {
-			throw new IllegalArgumentException("picture is null");
-		}
-		if (x == 0 || y == 0 || x == width() - 1 || y == height() - 1) {
-			return 1000.0;
-		} else {
-			front = picture.get(x - 1, y);
-			back = picture.get(x + 1, y);
-			dx = Math.pow((front.getRed() - back.getRed()), 2) + Math.pow((front.getGreen() - back.getGreen()), 2) + Math.pow((front.getBlue() - back.getBlue()), 2);
-			front = picture.get(x, y - 1);
-			back = picture.get(x, y + 1);
-			dy = Math.pow((front.getRed() - back.getRed()), 2) + Math.pow((front.getGreen() - back.getGreen()), 2) + Math.pow((front.getBlue() - back.getBlue()), 2);
-			return  Math.sqrt((dx + dy));
-		}
-	}
+                if (col != (w - 1)) {
+                    min = Math.min(min, energies[row - 1][col + 1]);
+                } else {
+                    min = min;
+                }
+                energies[row][col] += min;
+            }
+        }
 
-	// sequence of indices for horizontal seam
-	public int[] findHorizontalSeam() {
-		double[] distTo = new double[width() * height()];
-		int[] edgeTo = new int[width() * height()];
-		for (int col = 0; col < width(); col++) {
-			for (int row = 0; row < height(); row++) {
-				if (col == 0)
-					distTo[width() * row + col] = 0;
-				else
-					distTo[width() * row + col] = Double.POSITIVE_INFINITY;
-				edgeTo[width() * row + col] = -1;
-			}
-		}
-		for (int col = 0; col < width() - 1; col++) {
-			for (int row = 0; row < height(); row++) {
+    }
+    /**
+     * transpose matrix.
+     *
+     * @param      energies  The energies
+     *
+     * @return   transposed energies.
+     */
+    private double[][] transposeGrid(final double[][] energies) {
+        int h = energies.length, w = energies[0].length;
+        double[][] trEnergies = new double[w][h];
+        for (int row = 0; row < h; row++) {
+            for (int col = 0; col < w; col++) {
+                trEnergies[col][row] = energies[row][col];
+            }
+        }
+        return trEnergies;
+    }
+    /**
+     * vertical path.
+     *
+     * @param      energies  The energies
+     *
+     * @return vertical energies.
+     */
+    private int[] minVerticalPath(final double[][] energies) {
+        int h = energies.length, w = energies[0].length;
+        int[] path = new int[h];
 
-				// relax right-upper edge if it exists
-				if (row - 1 >= 0)
-					relaxEdge(width() * row + col, width() * (row - 1) + (col + 1), distTo, edgeTo);
-				// relax right-middle edge
-				relaxEdge(width() * row + col, width() * row + (col + 1), distTo, edgeTo);
-				// relax right-bottom edge if it exists
-				if (row + 1 <= height() - 1)
-					relaxEdge(width() * row + col, width() * (row + 1) + (col + 1), distTo, edgeTo);
-			}
-		}
-		double curMinDist = Double.POSITIVE_INFINITY;
-		int lastSeamPixel = -1;
-		for (int row = 0; row < height(); row++) {
-			if (distTo[(width() * row) + (width() - 1)] < curMinDist) {
-				curMinDist = distTo[(width() * row) + (width() - 1)];
-				lastSeamPixel = (width() * row) + (width() -1);//------------------->
-			}
-		}
-		int[] horSeam = new int[width()];
-		int curPixel = lastSeamPixel;
-		// int temp = lastSeamPixel;// my talent
+        topologicalSort(energies);
 
-		int i = horSeam.length - 1;
-		// while not beginning of the seam. LeftCol = -1
-		// horSeam[i] = ((curPixel) % (width()));
-		// 	curPixel = edgeTo[curPixel];
-		// int j = horSeam.length - 1;
+        // find the lowest element in last row
+        path[h - 1] = 0;
+        for (int i = 0; i < w; i++) {
+            if (energies[h - 1][i] < energies[h - 1][path[h - 1]]) {
+                path[h - 1] = i;
+            }
+        }
+        // trace path back to first row
+        // assuming we need the cheapest upper neighboring entry
+        for (int row = h - 2; row >= 0; row--) {
+            int col = path[row + 1];
+            // three neighboring, priority to center
+            path[row] = col;
+            if (col > 0 && energies[row][col - 1] < energies[row][path[row]]) {
+                path[row] = col - 1;
+            }
+            if (col < (w - 2) && energies[row][col + 1]
+                    < energies[row][path[row]]) {
+                path[row] = col + 1;
+            }
+        }
+        return path;
+    }
+    /**
+     * sequence of indices for horizontal seam.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int[] findHorizontalSeam() {
+        double[][] transposeEnergies = transposeGrid(initEnergies());
+        return minVerticalPath(transposeEnergies);
+    }
+    /**
+     * sequence of indices for vertical seam.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int[] findVerticalSeam() {
+        double[][] normalEnergies = initEnergies();
+        return minVerticalPath(normalEnergies);
+    }
+    /**
+     * Removes a horizontal seam.
+     * remove horizontal seam from current picture.
+     *
+     * @param      seam  The seam
+     */
+    public void removeHorizontalSeam(final int[] seam) {
+        if (height() <= 1 || !isValid(seam, width(), height() - 1)) {
+            throw new
+            java.lang.IllegalArgumentException("IllegalArgumentException");
+        }
+        Picture pic = new Picture(width(), height() - 1);
+        for (int w = 0; w < width(); w++) {
+            for (int h = 0; h < seam[w]; h++) {
+                pic.set(w, h, this.pict.get(w, h));
+            }
+
+            for (int h = seam[w] + 1; h < height(); h++) {
+                pic.set(w, h - 1, this.pict.get(w, h));
+            }
+
+        }
+        this.pict = pic;
+    }
+    /**
+     * Removes a vertical seam.
+     * remove vertical seam from current picture.
+     *
+     * @param      seam  The seam
+     */
+    public void removeVerticalSeam(final int[] seam) {
+        if (width() <= 1 || !isValid(seam, height(), width())) {
+            throw new
+            java.lang.IllegalArgumentException("IllegalArgumentException");
+        }
+        Picture pic = new Picture(width() - 1, height());
+        for (int h = 0; h < height(); h++) {
+            for (int w = 0; w < seam[h]; w++) {
+                pic.set(w, h, this.pict.get(w, h));
+            }
 
 
-		while (curPixel != -1) {
-			// System.out.println(curPixel+" ====== divide");
-			horSeam[i] = ((curPixel) / (width()));
-			// System.out.println(width()+"-----wid");
-			// System.out.println(horSeam[i]+"------------------");
-			curPixel = edgeTo[curPixel];
-			// System.out.println(curPixel+" ------- ");
-			i--;
-		}
-		// for (int k = horSeam.length - 1; k > 0; k--) {
-		// 	if(temp == -1){
-		// 		break;
-		// 	}
-		// 	horSeam[k] = ((temp) / (width()));
-		// 	temp = edgeTo[temp];
+            for (int w = seam[h] + 1; w < width(); w++) {
+                pic.set(w - 1, h, this.pict.get(w, h));
+            }
 
-			
-		// }
-		if(horSeam[0] != 0 || horSeam[0] !=1)
-		{
-		horSeam[0] += 1;
-
-		}
-
-		return horSeam;
-	}
-
-	// sequence of indices for vertical seam
-	public int[] findVerticalSeam() {
-		// transpose image
-		double[][] energycopy = energy;
-		transposeEnergy();
-		// find horizontal seam
-		int[] verSeam  = findHorizontalSeam();
-		// transpose back
-		energy = energycopy;
-		return verSeam;
-
-	}
-	public void transposeEnergy() {
-		double[][] transposedEnergy = new double[height()][width()];
-		for (int col = 0; col < width(); col++) {
-			for (int row = 0; row < height(); row++) {
-				transposedEnergy[row][col] = energy[col][row];
-			}
-		}
-		energy = transposedEnergy;
-	}
-
-	// remove horizontal seam from current picture
-	public void removeHorizontalSeam(int[] seam) {
-
-	}
-
-	// remove vertical seam from current picture
-	public void removeVerticalSeam(int[] seam) {
-
-	}
+        }
+        this.pict = pic;
+    }
+    /**
+     * Determines if valid.
+     *
+     * @param      a      { parameter_description }
+     * @param      len    The length
+     * @param      range  The range
+     *
+     * @return     True if valid, False otherwise.
+     */
+    private boolean isValid(final int[] a, final int len, final int range) {
+        if (a == null) {
+            return false;
+        }
+        if (a.length != len || a[0] < 0 || a[0] > range) {
+            return false;
+        }
+        for (int i = 1; i < len; i++) {
+            if (a[i] < Math.max(0, a[i - 1] - 1)
+                    || a[i] > Math.min(range, a[i - 1] + 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
+
--- ADS2_assignments/m10/Seam Carving/Solution.java
+++ ADS2_assignments/m10/Seam Carving/Solution.java
@@ -1,132 +1,166 @@
 import java.util.Scanner;
 import java.util.Arrays;
+/**
+ * Class for solution.
+ */
+public final class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+		//constructor not used.
+	}
+	/**
+	 * printEnergies funcytion to print the energies of each pixel.
+	 *
+	 * @param      fileName  The file name
+	 */
+	public static void printEnergies(final String fileName) {
+		Picture picture = new Picture(fileName);
+		StdOut.printf("image is %d pixels wide by %d pixels high.\n",
+		              picture.width(), picture.height());
 
-public class Solution {
+		SeamCarver sc = new SeamCarver(picture);
 
-    public static void printEnergies(String fileName) {
-        Picture picture = new Picture(fileName);
-        StdOut.printf("image is %d pixels wide by %d pixels high.\n", picture.width(), picture.height());
+		StdOut.printf("Printing energy calculated for each pixel.\n");
 
-        SeamCarver sc = new SeamCarver(picture);
+		for (int row = 0; row < sc.height(); row++) {
+			for (int col = 0; col < sc.width(); col++) {
+				StdOut.printf("%9.0f ", sc.energy(col, row));
+			}
+			StdOut.println();
+		}
+	}
+	/**
+	 * print seam function.
+	 * complexity is N^2.
+	 *
+	 * @param      carver     The carver
+	 * @param      seam       The seam
+	 * @param      direction  The direction
+	 */
+	public static void printSeam(final SeamCarver carver,
+	                             final int[] seam, final boolean direction) {
+		double totalSeamEnergy = 0.0;
 
-        StdOut.printf("Printing energy calculated for each pixel.\n");
+		for (int row = 0; row < carver.height(); row++) {
+			for (int col = 0; col < carver.width(); col++) {
+				double energy = carver.energy(col, row);
+				String marker = " ";
+				if ((direction && row == seam[col]) || (
+				            !direction   && col == seam[row])) {
+					marker = "*";
+					totalSeamEnergy += energy;
+				}
+				StdOut.printf("%7.2f%s ", energy, marker);
+			}
+			StdOut.println();
+		}
+		// StdOut.println();
+		StdOut.printf("Total energy = %f\n", totalSeamEnergy);
+		StdOut.println();
+		StdOut.println();
+	}
+	/**
+	 * Client program.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String cases = scan.nextLine();
+		SeamCarver seamCarver = null;
+		try {
+			switch (cases) {
+			case "width":
+				while (scan.hasNextLine()) {
+					String file = scan.nextLine();
+					seamCarver = new SeamCarver(
+					    new Picture("/Files/" + file));
+					System.out.println(seamCarver.width());
+				}
+				break;
 
-        for (int row = 0; row < sc.height(); row++) {
-            for (int col = 0; col < sc.width(); col++)
-                StdOut.printf("%9.0f ", sc.energy(col, row));
-            StdOut.println();
-        }
-    }
+			case "height":
+				while (scan.hasNextLine()) {
+					String file = scan.nextLine();
+					seamCarver = new SeamCarver(
+					    new Picture("/Files/" + file));
+					System.out.println(seamCarver.height());
+				}
+				break;
 
-    public static void printSeam(SeamCarver carver, int[] seam, boolean direction) {
-        double totalSeamEnergy = 0.0;
+			case "energy":
+				while (scan.hasNextLine()) {
+					String file = scan.nextLine();
+					printEnergies("/Files/" + file);
+				}
+				break;
 
-        for (int row = 0; row < carver.height(); row++) {
-            for (int col = 0; col < carver.width(); col++) {
-                double energy = carver.energy(col, row);
-                String marker = " ";
-                if ((direction == true && row == seam[col]) ||
-                        (direction == false   && col == seam[row])) {
-                    marker = "*";
-                    totalSeamEnergy += energy;
-                }
-                StdOut.printf("%7.2f%s ", energy, marker);
-            }
-            StdOut.println();
-        }
-        // StdOut.println();
-        StdOut.printf("Total energy = %f\n", totalSeamEnergy);
-        StdOut.println();
-        StdOut.println();
-    }
+			case "findVerticalSeam":
+				while (scan.hasNextLine()) {
+					String file = scan.nextLine();
+					seamCarver = new SeamCarver(
+					    new Picture("/Files/" + file));
+					System.out.println(Arrays.toString(
+					                       seamCarver.findVerticalSeam()));
+				}
+				break;
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String cases = scan.nextLine();
-        SeamCarver seamCarver = null;
-        try {
-            switch (cases) {
-                // case"":
-                // System.out.println("picture is null");
-                // break;
-            case "width":
-                while (scan.hasNextLine()) {
-                    String file = scan.nextLine();
-                    seamCarver = new SeamCarver(new Picture("/Files/" + file));
-                    System.out.println(seamCarver.width());
-                }
-                break;
+			case "findHorizontalSeam":
+				while (scan.hasNextLine()) {
+					String file = scan.nextLine();
+					seamCarver = new SeamCarver(
+					    new Picture("/Files/" + file));
+					System.out.println(Arrays.toString(
+					                       seamCarver.findHorizontalSeam()));
+				}
+				break;
 
-            case "height":
-                while (scan.hasNextLine()) {
-                    String file = scan.nextLine();
-                    seamCarver = new SeamCarver(new Picture("/Files/" + file));
-                    System.out.println(seamCarver.height());
-                }
-                break;
+			case "removeVerticalSeam":
+				while (scan.hasNextLine()) {
+					String file = scan.nextLine();
+					seamCarver = new SeamCarver(
+					    new Picture("/Files/" + file));
+					int[] vtseam = seamCarver.findVerticalSeam();
+					seamCarver.removeVerticalSeam(vtseam);
+					printSeam(seamCarver, vtseam, false);
+				}
+				break;
 
-            case "energy":
-                while (scan.hasNextLine()) {
-                    String file = scan.nextLine();
-                    printEnergies("/Files/" + file);
-                }
-                break;
+			case "removeHorizontalSeam":
+				while (scan.hasNextLine()) {
+					String file = scan.nextLine();
+					seamCarver = new SeamCarver(
+					    new Picture("/Files/" + file));
+					seamCarver.removeHorizontalSeam(
+					    seamCarver.findHorizontalSeam());
+					int[] hzseam = seamCarver.findHorizontalSeam();
+					seamCarver.removeHorizontalSeam(hzseam);
+					printSeam(seamCarver, hzseam, true);
+				}
+				break;
 
-            case "findVerticalSeam":
-                while (scan.hasNextLine()) {
-                    String file = scan.nextLine();
-                    seamCarver = new SeamCarver(new Picture("/Files/" + file));
-                    System.out.println(Arrays.toString(seamCarver.findVerticalSeam()));
-                }
-                break;
+			case "removeHorizontalSeam removeVerticalSeam":
+				while (scan.hasNextLine()) {
+					String file = scan.nextLine();
+					seamCarver = new SeamCarver(
+					    new Picture("/Files/" + file));
+					int[] hzseam = seamCarver.findHorizontalSeam();
+					seamCarver.removeHorizontalSeam(hzseam);
+					int[] vtseam = seamCarver.findVerticalSeam();
+					seamCarver.removeVerticalSeam(vtseam);
+					printEnergies("/Files/" + file);
+				}
+				break;
 
-            case "findHorizontalSeam":
-                while (scan.hasNextLine()) {
-                    String file = scan.nextLine();
-                    seamCarver = new SeamCarver(new Picture("/Files/" + file));
-                    System.out.println(Arrays.toString(seamCarver.findHorizontalSeam()));
-                }
-                break;
-
-            case "removeVerticalSeam":
-                while (scan.hasNextLine()) {
-                    String file = scan.nextLine();
-                    seamCarver = new SeamCarver(new Picture("/Files/" + file));
-                    int[] verticalSeam = seamCarver.findVerticalSeam();
-                    seamCarver.removeVerticalSeam(verticalSeam);
-                    printSeam(seamCarver, verticalSeam, false);
-                }
-                break;
-
-            case "removeHorizontalSeam":
-                while (scan.hasNextLine()) {
-                    String file = scan.nextLine();
-                    seamCarver = new SeamCarver(new Picture("/Files/" + file));
-                    seamCarver.removeHorizontalSeam(seamCarver.findHorizontalSeam());
-                    int[] horizontalSeam = seamCarver.findHorizontalSeam();
-                    seamCarver.removeHorizontalSeam(horizontalSeam);
-                    printSeam(seamCarver, horizontalSeam, true);
-                }
-                break;
-
-            case "removeHorizontalSeam removeVerticalSeam":
-                while (scan.hasNextLine()) {
-                    String file = scan.nextLine();
-                    seamCarver = new SeamCarver(new Picture("/Files/" + file));
-                    int[] horizontalSeam = seamCarver.findHorizontalSeam();
-                    seamCarver.removeHorizontalSeam(horizontalSeam);
-                    int[] verticalSeam = seamCarver.findVerticalSeam();
-                    seamCarver.removeVerticalSeam(verticalSeam);
-                    printEnergies("/Files/" + file);
-                }
-                break;
-
-            default:
-                seamCarver = new SeamCarver(null);
-                break;
-            }
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
+			default:
+				seamCarver = new SeamCarver(null);
+				break;
+			}
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
 }
+
